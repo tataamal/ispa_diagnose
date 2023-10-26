d8b80ee3bfc40781af31c31160aa2cd98d400e9b
tataamal <122243912+tataamal@users.noreply.github.com>
2023-10-27 00:36:21 +0700

    Add files via upload

---
+++ user/dashboard.php
@@ -0,0 +1,24 @@
+<?php 
+
+session_start();
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Ini adalah halaman user</h1>
+
+    <form method="post" action="../helper/auth/logout.php">
+        <button type="submit" name="logout" value="logout" class="btn btn-danger">Keluar</button>
+    </form>
+    
+</body>
+</html>
